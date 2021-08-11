7dbb5340f4d9cdd8322d7d112afaf7ab45671b8a
jsoto0025 <jasotov@eafit.edu.co>
2021-08-11 04:10:10 -0500

    Se mueve ExtendedFeatureModels a la carpetad Deprecated

---
+++ Deprecated/ExtendedFeatureModels/Dsl/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.ConstrainedExecution;
+
+#endregion
+
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly: AssemblyTitle(@"")]
+[assembly: AssemblyDescription(@"")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(@"jsoto0025")]
+[assembly: AssemblyProduct(@"SplModeling")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: System.Resources.NeutralResourcesLanguage("en")]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion(@"0.0.0.1")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: ReliabilityContract(Consistency.MayCorruptProcess, Cer.None)]
+
+//
+// Make the Dsl project internally visible to the DslPackage assembly
+//
+[assembly: InternalsVisibleTo(@"jsoto0025.SplModeling.ExtendedFeatureModels.DslPackage, PublicKey=002400000480000094000000060200000024000052534131000400000100010091A29C368993C534B4979F9E25946A65627DE77C97C8FB22C8F489BD8881133409FA2C66516AB6229446B9E64FB3D6F78FDFE2A6AC74365834A9846045020EEBD73554B5126A13BB9173FA872E63F9808FEE034BB2860DC279223AEE9F1BA2854B80E1A727B8A6ACF6F272ED16291E92DBA615DEA3271B0C80B453970873AFBB")]
